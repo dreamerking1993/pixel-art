c2e73016fcf149ce1ed6e8122bcbc91c0e7191bc
Sushant Khanal <dreamerking114@gmail.com>
2017-09-12 13:16:26 +0545

    grid without clickable cells: initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+$('button').on('click', function() {
+    $('.grid table').html('');
+    var rows = $('input[name="row"]').val(),
+      cols = $('input[name="column"]').val();
+    for (i = 0; i < rows; i++) {
+      $('.grid table').append('<tr></tr>')
+    }
+    for (t = 0; t < cols; t++) {
+      $('.grid table tr').each(function() {
+        $(this).append('<td></td>')
+      })
+    }
+  })

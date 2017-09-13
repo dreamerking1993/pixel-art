54bf9d24cb75c0d95115e91841c055ebbe86ba54
Sushant Khanal <dreamerking114@gmail.com>
2017-09-13 10:26:19 +0545

    beautified

--- app.js
+++ app.js
@@ -1,27 +1,27 @@
-$('#sizePicker').on('submit', function(){
-  makegrid();
-  return false;
+$('#sizePicker').on('submit', function() {
+    makegrid();
+    return false;
 });
 
 function makegrid() {
 
-$('button').on('click', function() {
-    $('.grid table').html('');
-    var rows = $('input[name="row"]').val(),
-      cols = $('input[name="column"]').val();
-    var i = 0;
-    while (i<rows) {
-      $('.grid table').append('<tr></tr>')
-      i=i+1;
-    }
-    for (t = 0; t < cols; t++) {
-      $('.grid table tr').each(function() {
-        $(this).append('<td>&nbsp;</td>')
-        $('#data tr td').on('click', function() {
-        $(this).css('background', $('#colorPicker').val());
-        });
+    $('button').on('click', function() {
+        $('.grid table').html('');
+        const rows = $('input[name="row"]').val(),
+            cols = $('input[name="column"]').val();
+        let i = 0;
+        while (i < rows) {
+            $('.grid table').append('<tr></tr>');
+            i = i + 1;
+        }
+        for (t = 0; t < cols; t++) {
+            $('.grid table tr').each(function() {
+                $(this).append('<td>&nbsp;</td>');
+                $('#data tr td').on('click', function() {
+                    $(this).css('background', $('#colorPicker').val());
+                });
 
-      })
-    }
-  })
+            });
+        }
+    });
 }
